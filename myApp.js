1d3024f354972ca88b89b3d1ad7f517ea73e8f5a
101jah <101jah@proton.me>
2024-06-18 18:16:01 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
 
-const mongoose = require("mongoose"); // Import Mongoose for MongoDB operations
 // MongoDB URI from environment variables
 const uri = process.env.MONGO_URI;
 
@@ -21,19 +21,22 @@ const personSchema = new mongoose.Schema({
     default: 0,
   },
   favoriteFoods: {
-    type: [String], // Array of strings for favorite foods
-    default: [], // Default empty array
+    type: [String],
+    default: [],
   },
 });
 
+// Create the Person model based on personSchema
 let Person = mongoose.model("Person", personSchema);
 
+// Function to create and save a single person document
 const createAndSavePerson = function (done) {
   const johnDoe = new Person({
     name: "John Doe",
     age: 100,
     favoriteFoods: ["eggs", "fish", "fresh fruit"],
   });
+
   // Save the new person document to the database
   johnDoe.save(function (err, data) {
     if (err) return done(err);
@@ -41,11 +44,6 @@ const createAndSavePerson = function (done) {
   });
 };
 
-
-
-
-
-// Function to create multiple people documents
 // Function to find people by name
 const findPeopleByName = function (personName, done) {
   // Use the find method to query people by name
@@ -55,8 +53,7 @@ const findPeopleByName = function (personName, done) {
   });
 };
 
-// Function to find one person by favorite fromCharCode(:
-// );
+// Function to find one person by favorite food
 const findOneByFood = function (food, done) {
   // Use findOne method to find a person by their favorite food
   Person.findOne({ favoriteFoods: food }, function (err, personFound) {
@@ -74,62 +71,91 @@ const findPersonById = function (personId, done) {
   });
 };
 
-
-
-
+// Function to find a person by ID, add a favorite food, and save
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
+
+  // Step 1: Find the person by _id
   Person.findById(personId, function (err, person) {
     if (err) return done(err);
-    done(null, person); // Invoke callback with found person
 
+    if (!person) {
+      console.log("Person not found");
+      return done(new Error("Person not found"));
+    }
+
+    // Step 2: Modify the favoriteFoods array
     person.favoriteFoods.push(foodToAdd);
+
+    // Step 3: Save the updated person
     person.save(function (err, data) {
       if (err) return done(err);
       done(null, data); // Invoke callback with saved person
     });
   });
-
-  
-
-  done(null /*, data*/);
 };
 
+// Function to find and update a person's age
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  // Use findOneAndUpdate to find and update a person by their name
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet },
+    { new: true },
+    function (err, updatedPerson) {
+      if (err) return done(err);
+      done(null, updatedPerson); // Invoke callback with updated person
+    }
+  );
 };
 
+// Function to remove a person by ID
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  // Use findByIdAndRemove to remove a person by their MongoDB _id
+  Person.findByIdAndRemove(personId, function (err, removedPerson) {
+    if (err) return done(err);
+    done(null, removedPerson); // Invoke callback with removed person
+  });
 };
 
+// Function to remove people by name
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+  // Use deleteMany to remove people by name
+  Person.deleteMany({ name: nameToRemove }, function (err, result) {
+    if (err) return done(err);
+    done(null, result); // Invoke callback with delete operation result
+  });
 };
 
+// Function for querying using chaining methods
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  // Use chaining methods to query people by favorite food and apply sorting, limiting, and selecting fields
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort("name")
+    .limit(2)
+    .select("-age")
+    .exec(function (err, data) {
+      if (err) return done(err);
+      done(null, data); // Invoke callback with query result
+    });
 };
 
-/** **Well Done !!**
-/* You completed these challenges, let's go celebrate !
- */
-
-//----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
-
-exports.PersonModel = Person;
-exports.createAndSavePerson = createAndSavePerson;
-exports.findPeopleByName = findPeopleByName;
-exports.findOneByFood = findOneByFood;
-exports.findPersonById = findPersonById;
-exports.findEditThenSave = findEditThenSave;
-exports.findAndUpdate = findAndUpdate;
-exports.removeById = removeById;
-exports.removeManyPeople = removeManyPeople;
-exports.queryChain = queryChain;
+// Export functions and Person model for use in other modules
+module.exports = {
+  PersonModel: Person,
+  createAndSavePerson,
+  findPeopleByName,
+  findOneByFood,
+  findPersonById,
+  findEditThenSave,
+  findAndUpdate,
+  removeById,
+  removeManyPeople,
+  queryChain,
+};
